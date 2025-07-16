7de94b53ea26e0cca282a3e8043c8142f9cc95f2
Custromix <75646944+Custromix@users.noreply.github.com>
2025-07-16 22:48:04 +0200

    [CHORE] & [ADDED] Change folder path, clean hierarchie & add real character and gamemode

---
+++ Source/FormationHicham/Gameplay/Character/HichamPlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HichamPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORMATIONHICHAM_API AHichamPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
